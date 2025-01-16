bd98c263de47f5d25a43cbfade8236f0480a88a1
MarkSian <merksianipar@gmail.com>
2025-01-15 21:15:02 -0700

    config folder and server.ts
    setup connection.ts and server.ts

---
+++ src/server.ts
@@ -0,0 +1,20 @@
+import express from 'express';
+import connectDB from './config/connection';
+
+const app = express();
+const PORT = 3001; // Port number
+
+//Middleware
+app.use(express.json());
+
+// MongoDB connection
+connectDB();
+
+// Routes *uncomment when routes are created
+//app.use('/api/users', userRoutes);
+//app.use('/api/routes', thoughtRoutes); 
+
+// Server listening
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
