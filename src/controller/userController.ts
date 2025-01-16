21fc2437ba41ffcbadaf9ac574c01317eb5b1dc5
MarkSian <merksianipar@gmail.com>
2025-01-16 16:55:18 -0700

    Update userController.ts

--- src/controller/userController.ts
+++ src/controller/userController.ts
@@ -35,3 +35,40 @@ export const getUserById = async (req: Request, res: Response): Promise<void> =>
         res.status(500).json(err);
     }
 };
+
+//Create A User
+export const createUser = async (req: Request, res: Response) => {
+    try {
+        const user = await User.create(req.body);
+        res.status(201).json(user);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+};
+
+//Delete A User
+export const deleteUser = async (req: Request, res: Response): Promise<void> => {
+    try {
+        const user = await User.findByIdAndDelete(req.params.userId);
+        if (!user) {
+            res.status(404).json({ message: 'No user found with this id!' });
+            return;
+        }
+    } catch (err) {
+        res.status(500).json(err);
+    }
+};
+
+//Update A User
+export const updateUser = async (req: Request, res: Response): Promise<void> => {
+    try {
+        const user = await User.findByIdAndUpdate(req.params.userId, req.body, { new: true });
+        if (!user) {
+            res.status(404).json({ message: 'No user found with this id!' });
+            return;
+        }
+        res.status(200).json(user);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+};
